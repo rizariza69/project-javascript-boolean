a400a46e2a4fbb0a6f3bd4fd898f3dc43a31af24
Muhammad Riza <riza.riza69@gmail.com>
2019-04-05 08:50:39 +0700

    project javscript boolean

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log(true)
+console.log(false)
+console.log(true, false)
+
+const toggle = false
+
+console.log(`The lamp's toggle is ${toggle}`)
+
+alert(`The lamp's toggle is ${toggle}`)
+
+
+let isNightTime = true;
+
+if (isNightTime) {
+  console.log('Turn on the lights!');
+} else {
+  console.log('Turn off the lights!');
+}
